1c1f650237d7c8bdaf944f2c831a386a2e454a61
Antonio Martinez <a.martinez@scmsi.es>
2019-05-19 08:29:31 +0200

    nuevo archivo:  tables/config.yaml 	nuevo archivo:  tables/informixtables 	nuevo archivo:  tables/informixtables.go 	nuevo archivo:  tables/odbc.ini 	nuevo archivo:  tables/tables.go 	modificado:     informixcollector

---
+++ tables/informixtables.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"database/sql"
+	"flag"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"sync"
+
+	_ "github.com/alexbrainman/odbc"
+	"gopkg.in/yaml.v2"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+	configfile = flag.String("configfile", "config.yaml", "Configuration File")
+	puerto     = flag.String("port", "8088", "Listen Port")
+	Instances  *Configuration
+)
+
+type Instance struct {
+	Name           string `yaml:"name"`
+	Informixserver string `yaml:"informixserver"`
+	User           string `yaml:"user"`
+	Password       string `yaml:"password"`
+	db             *sql.DB
+}
+
+type Configuration struct {
+	Servers []Instance `yaml:"servers"`
+	Custom  []struct {
+		Query    string `yaml:"query"`
+		Response string `yaml:"response"`
+	} `yaml:"custom"`
+}
+type metric struct {
+	Name string
+	Help string
+}
+
+type Coleccion interface {
+	Scrape() error
+	Collect(chan<- prometheus.Metric)
+	Describe(chan<- *prometheus.Desc)
+}
+
+type Exporter struct {
+	m        sync.Mutex
+	metricas []Coleccion
+}
+
+func NewExporter() *Exporter {
+
+	e := &Exporter{
+
+		metricas: []Coleccion{
+			NewtablesMetrics(),
+		},
+	}
+
+	return e
+
+}
+
+func (e *Exporter) scrape() {
+
+	for _, m := range e.metricas {
+		err := m.Scrape()
+		if err != nil {
+			log.Println("Error in scrape data")
+		}
+
+	}
+
+}
+
+func (e *Exporter) Describe(ch chan<- *prometheus.Desc) {
+
+	for _, m := range e.metricas {
+		m.Describe(ch)
+	}
+
+}
+
+func (e *Exporter) Collect(ch chan<- prometheus.Metric) {
+
+	e.m.Lock()
+	defer e.m.Unlock()
+	e.scrape()
+	for _, m := range e.metricas {
+		m.Collect(ch)
+	}
+
+}
+
+func loadConfig(filename *string) (*Configuration, error) {
+
+	bytes, err := ioutil.ReadFile(*filename)
+	if err != nil {
+		return &Configuration{}, err
+	}
+
+	var c Configuration
+	err = yaml.Unmarshal(bytes, &c)
+	if err != nil {
+
+		return &Configuration{}, err
+	}
+
+	return &c, nil
+}
+
+func main() {
+
+	flag.Parse()
+	var err error
+	log.Println("Cargando fichero de configuracion:")
+	Instances, err = loadConfig(configfile)
+	if err != nil {
+		log.Fatal("Error en  fichero Yaml:", err)
+
+	}
+
+	exporter := NewExporter()
+	prometheus.MustRegister(exporter)
+
+	log.Println("Arrancando servidor V0.1 en puerto:", *puerto)
+	http.Handle("/metrics", promhttp.Handler())
+	log.Fatal(http.ListenAndServe(":"+*puerto, nil))
+	os.Exit(0)
+
+}
---
+++ tables/tables.go
@@ -0,0 +1,168 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"sync"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+type TablesMetrics struct {
+	mutex   sync.Mutex
+	metrics map[string]*prometheus.GaugeVec
+}
+
+var (
+	tableMetrics = map[string]metric{
+		"extents":     metric{Name: "extents", Help: "Total extents"},
+		"table_pages": metric{Name: "table_pages", Help: "Total pages"},
+	}
+)
+
+func NewtablesMetrics() *TablesMetrics {
+
+	e := TablesMetrics{metrics: map[string]*prometheus.GaugeVec{}}
+	for key, _ := range tableMetrics {
+		e.metrics[key] = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+			Namespace: "informix",
+			Name:      key,
+			Help:      key},
+			[]string{"informixserver", "table"})
+	}
+	return &e
+}
+
+func querytables(p *TablesMetrics, Instancia Instance) error {
+
+	var err error
+
+	var (
+		name  string
+		value float64
+		table string
+		pages float64
+	)
+
+	rows, err := Instancia.db.Query(`select tabname[1,20], count(*)
+	from sysmaster:sysextents e
+	group by dbsname,tabname
+	having count(*)>150
+	order by 1,2
+	`)
+
+	if err != nil {
+		log.Println("Error in  Query extents: \n", err)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+
+		err := rows.Scan(&name, &value)
+
+		if err != nil {
+			log.Println("Error in Scan", err)
+			return err
+		}
+		//	if _, ok := p.metrics[strings.TrimSpace(name)]; ok {
+
+		//	p.metrics[strings.TrimSpace(name)].WithLabelValues(Instancia.Name, name).Set(value)
+		p.metrics["extents"].WithLabelValues(Instancia.Name, name).Set(value)
+
+	}
+	rows.Close()
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rows, err = Instancia.db.Query(`select  tabname,size from sysextents
+	group by 1,2
+	having sum(size) > 10000000
+	`)
+
+	if err != nil {
+		log.Println("Error in  Query pages: \n", err)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+
+		err := rows.Scan(&table, &pages)
+
+		if err != nil {
+			log.Println("Error in Scan", err)
+			return err
+		}
+		//	if _, ok := p.metrics[strings.TrimSpace(name)]; ok {
+
+		//	p.metrics[strings.TrimSpace(name)].WithLabelValues(Instancia.Name, name).Set(value)
+		p.metrics["table_pages"].WithLabelValues(Instancia.Name, table).Set(pages)
+
+	}
+	rows.Close()
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return nil
+
+}
+
+func (p *TablesMetrics) Scrape() error {
+	p.mutex.Lock()
+	defer p.mutex.Unlock()
+
+	var err error
+
+	for m, _ := range Instances.Servers {
+		connect := "DSN=" + Instances.Servers[m].Informixserver
+		log.Println("Conectando a DSN", connect)
+		for intentos := 0; intentos < 5; intentos++ {
+
+			Instances.Servers[m].db, err = sql.Open("odbc", connect)
+			err = Instances.Servers[m].db.Ping()
+			if err != nil {
+				time.Sleep(1 * time.Second)
+
+			} else {
+				break
+			}
+		}
+
+		if err != nil {
+			Instances.Servers = append(Instances.Servers[:m], Instances.Servers[m+1:]...)
+			log.Println("Error in Open Database: ", err)
+		}
+	}
+
+	defer func() {
+		for m, _ := range Instances.Servers {
+			log.Println("Cerrando DSN", m)
+			Instances.Servers[m].db.Close()
+		}
+	}()
+	for m, _ := range Instances.Servers {
+		log.Println("Ejecutando Querys:", m)
+		querytables(p, Instances.Servers[m])
+	}
+	return nil
+}
+
+func (p *TablesMetrics) Describe(ch chan<- *prometheus.Desc) {
+	for _, m := range p.metrics {
+		m.Describe(ch)
+	}
+}
+
+func (p *TablesMetrics) Collect(ch chan<- prometheus.Metric) {
+
+	for _, m := range p.metrics {
+		m.Collect(ch)
+	}
+
+}
